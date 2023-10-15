c67c50a3ed22d2c8bbc48f35a209aab070d4aa72
Pratik Dighole <pratikdighole2019@gmail.com>
2023-10-15 20:00:45 +0530

    commi

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import InfoPage from './components/InfoPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import React from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import InfoPage from "./components/InfoPage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" Component={HomePage} />
         <Route path="/product/:id" Component={InfoPage} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
